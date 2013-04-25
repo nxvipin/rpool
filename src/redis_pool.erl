384086c695f9ce98d29f0ae858f31747b08dd46a
Vipin Nair <swvist@gmail.com>
2013-04-26 00:54:29 +0530

    Adding few Set operations API

--- src/redis_pool.erl
+++ src/redis_pool.erl
@@ -11,16 +11,36 @@
 
 
 %% API
--export([get/2, set/3]).
+-export([
+	 get/2,
+	 set/3,
+	 sadd/3,
+	 srem/3,
+	 sismember/3
+	]).
 
+
+						% Generic Query
 q(PoolName, Query)->
     Worker = poolboy:checkout(PoolName),
     Reply = gen_server:call(Worker, {rquery, Query}),
     poolboy:checkin(PoolName, Worker),
     Reply.
 
+%% String Operations
 get(PoolName, Key) ->
     q(PoolName, ["GET", Key]).
 
 set(PoolName, Key, Value) ->
     q(PoolName, ["SET", Key, Value]).
+
+
+%% Set Operations
+sadd(PoolName, Key, Value) ->
+    q(PoolName, ["SADD", Key, Value]).
+
+srem(PoolName, Key, Value) ->
+    q(PoolName, ["SREM", Key, Value]).
+
+sismember(PoolName, Key, Value) ->
+    q(PoolName, ["SISMEMBER", Key, Value]).
